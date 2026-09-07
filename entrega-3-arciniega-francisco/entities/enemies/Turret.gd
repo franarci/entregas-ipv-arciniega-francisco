c0fd74428693efa689d972f85b4ea9053470dd52
franarci <fran.arciniega96@gmail.com>
2026-09-07 17:03:08 -0300

    torretas sin inyeccion de dependencias

--- entrega-3-arciniega-francisco/entities/enemies/Turret.gd
+++ entrega-3-arciniega-francisco/entities/enemies/Turret.gd
@@ -5,20 +5,31 @@ extends Sprite2D
 
 @export var projectile_scene: PackedScene
 
-var player: Node2D
 var projectile_container: Node
 
-func initialize(turret_pos: Vector2, p_player: Node2D, p_projectile_container: Node) -> void:
-	global_position = turret_pos
-	self.player = p_player
-	self.projectile_container = p_projectile_container
-	fire_timer.connect("timeout", fire_at_player)
-	fire_timer.start()
+var target: Node2D
 
-func fire_at_player() -> void:
+func _ready() -> void:
+	fire_timer.connect("timeout", fire_at_target)
+
+func initialize(turret_pos: Vector2, p_projectile_container: Node) -> void:
+	global_position = turret_pos
+	self.projectile_container = p_projectile_container
+	
+
+func fire_at_target() -> void:
 	var proj_instance = projectile_scene.instantiate()
 	proj_instance.initialize(
 		projectile_container,
 		fire_position.global_position,
-		fire_position.global_position.direction_to(player.global_position)
+		fire_position.global_position.direction_to(target.global_position)
 	)
+
+
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	target = body
+	fire_timer.start()
+
+
+func _on_detection_area_body_exited(_body: Node2D) -> void:
+	fire_timer.stop()
